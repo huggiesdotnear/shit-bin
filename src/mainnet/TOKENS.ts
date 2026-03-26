08208498cd8e3fd8fa59b3c3af67329f8053aa31
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-25 20:50:59 -0500

    re

---
+++ src/mainnet/TOKENS.ts
@@ -0,0 +1,35 @@
+// =====================================
+// HUGGIES WHITELISTED TOKENS
+// DYOR BEFORE TRADING ANY TOKEN
+// =====================================
+// TOKENS
+export const TOKENS = {
+  SHIT: "shit-1170.meme-cooking.near",
+  // other
+  CRANS: "crans.tkn.near",
+  SHITZU: "token.0xshitzu.near",
+  BLACKDRAGON: "blackdragon.tkn.near",
+  PUMP: "token.pumpopoly.near",
+  ZEC: "zec.omft.near",
+  USDT: "usdt.tether-token.near",
+  USDC: "17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1",
+  NBTC: "nbtc.bridge.near",
+  AURORA: "aaaaaa20d9e0e2461697782ef11675f668207961.factory.bridge.near",
+  // near
+  WNEAR: "wrap.near",
+  NEAR: "near", // native near
+  STNEAR: "meta-pool.near",
+  rNEAR: "lst.rhealab.near",
+  // meme-cooking
+  SIN: "sin-339.meme-cooking.near",
+  JAMBO: "jambo-1679.meme-cooking.near",
+  XPOST: "xpost-1634.meme-cooking.near",
+  VOTE: "vote-1737.meme-cooking.near",
+  NILLIONS: "n-1813.meme-cooking.near",
+  ZOLANEAR: "zolanear-1726.meme-cooking.near",
+  DOGSHIT: "dogshit-1408.meme-cooking.near",
+  JLU: "jlu-1018.meme-cooking.near",
+  NOEAR: "noear-324.meme-cooking.near",
+  POPPY: "poppy-0.meme-cooking-test.near",
+};
+// =====================================
