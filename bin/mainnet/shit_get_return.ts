2f6b6ba33ff9e0eb96f66bb8bc3875680456487c
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-25 22:07:16 -0500

    Update shit_get_return.ts

--- bin/mainnet/shit_get_return.ts
+++ bin/mainnet/shit_get_return.ts
@@ -19,8 +19,18 @@ const WNEAR_IN_SHIT_OUT_RESULT = await ref_get_return_function(
   WNEAR_START_AMOUNT,
   MAINNET_TOKENS.SHIT
 );
-console.log("SHIT OUT:", WNEAR_IN_SHIT_OUT_RESULT);
+console.log("SHIT BUY QUOTE:", WNEAR_IN_SHIT_OUT_RESULT);
 console.log("==============================================");
+console.log("WNEAR - SHIT - ZEC :: ARB")
+// GET SHIT BUY QUOTE
+const WNEAR_IN_SHIT_OUT_ZERO_FEE_RESULT = await ref_get_return_function(
+  near_kit_env,
+  rhea_contractId_for_network_env,
+  MAINNET_POOLS.SHIT_WNEAR_FEE_10,
+  MAINNET_TOKENS.WNEAR,
+  WNEAR_START_AMOUNT,
+  MAINNET_TOKENS.SHIT
+);
 console.log("==============================================");
 console.log("==============================================");
 // =====================================
