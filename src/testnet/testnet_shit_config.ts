9f62d0edf00550ac412f4cb59efef4a5cec54540
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-25 20:05:01 -0500

    Create testnet_shit_config.ts

---
+++ src/testnet/testnet_shit_config.ts
@@ -0,0 +1,18 @@
+// =====================================
+// POOLS
+export const TESTNET_POOLS = {
+  SHIT_WNEAR: 2459,
+};
+// =====================================
+// TOKENS
+export const TESTNET_TOKENS = {
+  SHIT: "shit-237.factory.v10.meme-cooking.testnet",
+  WNEAR: "wrap.testnet",
+};
+export const TESTNET_TOKEN_DECIMALS = {
+  [TESTNET_TOKENS.SHIT]: 18,
+  [TESTNET_TOKENS.WNEAR]: 24,
+};
+// =====================================
+// =====================================
+// copyright 2026 by sleet.near
