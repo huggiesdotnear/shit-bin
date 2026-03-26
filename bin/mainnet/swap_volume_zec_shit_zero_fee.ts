823d0dd442849c15df0d67e9984a860a1d2ce166
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-26 14:27:29 -0500

    Update swap_volume_zec_shit_zero_fee.ts

--- bin/mainnet/swap_volume_zec_shit_zero_fee.ts
+++ bin/mainnet/swap_volume_zec_shit_zero_fee.ts
@@ -9,14 +9,14 @@ import {
 // =====================================
 // required zec in rhea contract
 const amount_in = "820200";
-// const min_amount_out = "0"
+const min_amount_out = "820195";
 // =====================================
 console.log(
   await volume_swap_zec_shit_zero_fee_fun(
     near_kit_env,
     rhea_contractId_for_network_env,
     amount_in,
-    amount_in,
+    min_amount_out,
     referral_id_for_network_env,
   ),
 );
