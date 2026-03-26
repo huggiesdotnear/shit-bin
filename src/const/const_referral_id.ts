05b98dcd11a632e51659d476e9d900446d35a5c1
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-26 07:48:44 -0500

    Update const_referral_id.ts

--- src/const/const_referral_id.ts
+++ src/const/const_referral_id.ts
@@ -1,4 +1,7 @@
-import { MY_ENV_NEAR_networkId } from "@sleet-js/near-kit-custom-client-setup-with-other-custom-functions";
+import {
+  MY_ENV_NEAR_networkId,
+  getStoredNetworkId,
+} from "@sleet-js/near-kit-custom-client-setup-with-other-custom-functions";
 // =====================================
 const referral_id_testnet = "ref.huggies.testnet";
 const referral_id_mainnet = "ref.huggies.near";
@@ -8,4 +11,15 @@ export const referral_id_for_network_env =
   MY_ENV_NEAR_networkId === "testnet"
     ? referral_id_testnet
     : referral_id_mainnet;
+// ===========================================
+// web
+export function referral_id_for_network_web() {
+  const networkId = getStoredNetworkId();
+  const referral_id =
+    networkId === "testnet"
+      ? referral_id_testnet
+      : referral_id_mainnet;
+  console.log("referral id ", referral_id);
+  return referral_id;
+}
 // ===========================================
