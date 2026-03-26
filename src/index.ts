9833296648769fa62b617808f775e5c2cb8356a9
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-25 20:07:14 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+// =====================================
+// shit-bin
+// =====================================
+// mainnet
+// =====================================
+// testnet
+export {
+  TESTNET_POOLS,
+  TESTNET_TOKENS,
+  TESTNET_TOKEN_DECIMALS,
+} from "./testnet/testnet_shit_config";
+// =====================================
