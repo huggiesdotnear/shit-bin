019de7925feeecf3a0a87a18982c592f45811114
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-26 14:21:47 -0500

    re

--- bin/README.md
+++ bin/README.md
@@ -22,6 +22,7 @@ bun run bin/mainnet/rhea_wrap_and_deposit.ts
 # volume
 bun run bin/mainnet/swap_volume_wnear_shit_zero_fee.ts
 bun run bin/mainnet/swap_volume_wnear_zec_zero_fee.ts
+bun run bin/mainnet/swap_volume_zec_shit_zero_fee.ts
 ```
 
 ---
--- src/index.ts
+++ src/index.ts
@@ -16,4 +16,5 @@ export { referral_id_for_network_env } from "./const/const_referral_id";
 // =====================================
 export { volume_swap_wnear_shit_zero_fee_fun } from "./fun/swap_volume_wnear_shit_zero_fee";
 export { volume_swap_wnear_zec_zero_fee_fun } from "./fun/swap_volume_wnear_zec_zero_fee";
+export { volume_swap_zec_shit_zero_fee_fun } from "./fun/swap_volume_zec_shit_zero_fee";
 // =====================================
