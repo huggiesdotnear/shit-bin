628c938ca5cbbf8f19eb03d5cf3e516d7c0748b4
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-05-31 23:07:07 -0500

    Update HOLDERS.ts

--- src/const/mainnet/HOLDERS.ts
+++ src/const/mainnet/HOLDERS.ts
@@ -1,3 +1,41 @@
 // =====================================
 // HOLDERS
 // =====================================
+// Known accounts mapping
+export const KNOWN_HOLDERS_ACCOUNTS: Record<string, "dev" | "dex" | "vault" | "nft" | "burn"> = {
+  "sleet.near": "dev",
+  "vault.huggies.near": "vault",
+  "growth.huggies.near": "vault",
+  "future.huggies.near": "vault",
+  "diaper.huggies.near": "vault",
+  "baby.huggies.near": "vault",
+  "huggies.sputnik-dao.near": "vault",
+  "ref.huggies.near": "vault",
+  // "shitbyhuggies.tipbot.intear.near": "vault",
+  "huggies.near": "vault",
+  "sleet_mainnet_test_yes_doxing_myself_sorry_for_rugs.near": "dev",
+  "nonresistant.near": "dev",
+  "nonresistantreally.tg": "dev",
+  "frozen_bot2.user.intear.near": "dev",
+  // "rug-testing.near": "dev", i do not own this account
+  "v2.ref-finance.near": "dex",
+  "veax.near": "dex",
+  "dex.intear.near": "dex",
+  "meme-cooking.near": "dex",
+  // nft
+  "notdone.near": "nft",
+  "outwit.near": "nft",
+  "fediverse.near": "nft",
+  "waffel.near": "nft",
+  "utes.near": "nft",
+  "powwow.near": "nft",
+  "nearnexus.near": "nft",
+  "hodldogshit.near": "nft",
+  "sofo.near": "nft",
+  "jeetersnotallowed.near": "nft",
+  "ironclaw.near": "nft",
+  "chicago.near": "nft",
+  "near-social-js.near": "nft",
+  "burn.sleet.near": "burn",
+};
+// =====================================
